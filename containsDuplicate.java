9e2cf79c3f1676db46823a13d472eccbd839456a
radhika228 <radhikagupta0891@gmail.com>
2025-12-04 13:48:23 +0000

    contains duplicate

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -1,14 +1,14 @@
 //leetcode 217
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer>Map = new HashMap<>();
+        Map<Integer, Integer>Map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int num = nums[i];
-            if(Map.contains(num)){
+            if(Map.containsKey(num)){
                 return true;
             }
             else{
-                Map.add(num);
+                Map.put(num,1);
             
             }
 
